5c1c26370ec6b7a44a88d051042b343136511743
dhinesh2595 <114347908+dhinesh2595@users.noreply.github.com>
2022-10-27 00:59:33 +0530

    Add files via upload

---
+++ database/migrations/2022_10_25_182751_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+        $table->id();
+         $table->string('name')->nullable();
+         $table->string('company_name')->nullable();
+         $table->string('salary')->nullable();
+         $table->date('joined_date')->nullable();
+         $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
